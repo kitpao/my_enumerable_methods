e942c6ebd9c9f761bb723820cdada63cee2c5980
kitpao <kitpao91@gmail.com>
2019-12-10 12:14:35 -0600

    add my_all? method

--- my_methods.rb
+++ my_methods.rb
@@ -18,8 +18,22 @@ module Enumerable
   def my_select
     return to_enum(:my_each) unless block_given?
 
-    newArr = []
-    self.my_each { |item| newArr << item if yield item }
-    newArr
+    new_arr = []
+    my_each { |item| new_arr << item if yield item }
+    new_arr
+  end
+
+  def my_all?(arg = nil)
+    if block_given?
+      counter = 0
+      my_each { |item| counter += 1 unless yield item }
+      counter.zero?
+    elsif arg.nil?
+      my_all? { |item| item }
+    else
+      # rubocop:disable Style/CaseEquality
+      my_all? { |item| arg === item }
+      # rubocop:enable Style/CaseEquality
+    end
   end
 end
