80e02e4bb0ec9d33ab6d25dd22bd9bb275db49cc
kitpao <kitpao91@gmail.com>
2019-12-10 10:47:18 -0600

    add my_select method

--- my_methods.rb
+++ my_methods.rb
@@ -9,6 +9,17 @@ module Enumerable
   end
 
   def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+
     size.times { |pos| yield self[pos], pos }
+    self
+  end
+
+  def my_select
+    return to_enum(:my_each) unless block_given?
+
+    newArr = []
+    self.my_each { |item| newArr << item if yield item }
+    newArr
   end
 end
